6fd5e6635ff5b52d49b851224944f88ee0ecd760
Daniel Lopes Ferreira <daniel.lopes.ferreira98@gmail.com>
2021-08-20 18:31:13 -0300

    test

--- bin/install-mg2.sh
+++ bin/install-mg2.sh
@@ -2,10 +2,10 @@
 
 cd magento2
 bin/magento --version
-# sudo chmod -Rf 777 var/ pub/ generated/ app/etc/env.php
-#### php -d memory_limit=5G bin/magento indexer:reindex
-# bin/magento setup:upgrade
-# php -d memory_limit=5G bin/magento setup:di:compile
-# bin/magento cache:clean
-# bin/magento cache:flush
-# sudo chmod -Rf 777 var/ pub/ generated/ app/etc/env.php
+sudo chmod -Rf 777 var/ pub/ generated/ app/etc/env.php
+php -d memory_limit=5G bin/magento indexer:reindex
+bin/magento setup:upgrade
+php -d memory_limit=5G bin/magento setup:di:compile
+bin/magento cache:clean
+bin/magento cache:flush
+sudo chmod -Rf 777 var/ pub/ generated/ app/etc/env.php
