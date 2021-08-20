f88c5119b879f752168c00928a41313dcca7f12c
Douglas <douglas.correia@mercadolivre.com>
2021-08-20 19:39:17 -0300

    Update install-mg2.sh

--- bin/install-mg2.sh
+++ bin/install-mg2.sh
@@ -5,8 +5,8 @@ bin/magento --version
 sudo chmod -Rf 777 var/ pub/ generated/ app/etc/env.php
 php -d memory_limit=5G bin/magento indexer:reindex
 bin/magento setup:upgrade
+bin/magento module:enable --all
 php -d memory_limit=5G bin/magento setup:di:compile
 bin/magento cache:clean
 bin/magento cache:flush
-bin/magento module:enable --all
 sudo chmod -Rf 777 var/ pub/ generated/ app/etc/env.php
