6e0811e72f108ff7ca9c7b6b5b0b9f6aaa01db70
Daniel Lopes Ferreira <daniel.lopes.ferreira98@gmail.com>
2021-08-20 17:53:22 -0300

    test

--- bin/install-mg2.sh
+++ bin/install-mg2.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 cd magento2
+bin/magento --version
 sudo chmod -Rf 777 var/ pub/ generated/ app/etc/env.php
-php -d memory_limit=5G bin/magento indexer:reindex
+# php -d memory_limit=5G bin/magento indexer:reindex
 bin/magento setup:upgrade
 php -d memory_limit=5G bin/magento setup:di:compile
 bin/magento cache:clean
