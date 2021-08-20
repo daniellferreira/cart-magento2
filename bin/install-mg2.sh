381d6da4884398c9d8ad35c4e56b863543049e13
Daniel Lopes Ferreira <daniel.lopes.ferreira98@gmail.com>
2021-08-20 17:37:22 -0300

    added install mg2 script

---
+++ bin/install-mg2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo chmod -Rf 777 var/ pub/ generated/ app/etc/env.php
+php -d memory_limit=5G bin/magento indexer:reindex
+bin/magento setup:upgrade
+php -d memory_limit=5G bin/magento setup:di:compile
+bin/magento cache:clean
+bin/magento cache:flush
+sudo chmod -Rf 777 var/ pub/ generated/ app/etc/env.php
