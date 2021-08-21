fd5890cc798110e31a4fce9911e01562ecd2fe4c
Douglas <douglas.correia@mercadolivre.com>
2021-08-20 21:07:11 -0300

    Update install-mp-plugin.sh

--- bin/install-mp-plugin.sh
+++ bin/install-mp-plugin.sh
@@ -1,7 +1,7 @@
 cd magento2/app/code &&
 curl -LO https://github.com/PluginAndPartners/cart-magento2/archive/develop.zip &&
 unzip develop.zip &&
-mv cart-magento2-develop magento2/app/code &&
+ls -la
 cd ~/magento2 &&
 bin/magento setup:upgrade && 
 bin/magento cache:clean
